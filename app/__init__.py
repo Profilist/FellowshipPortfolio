1ed29fd41c564b3650f6459531a3b8e090e4b06b
Larris Xie <larris.xie@gmail.com>
2025-07-10 11:35:06 -0400

    Adds API for timeline posts with POST and GET methods, includes database integration and a curl test script

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,27 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
+from playhouse.shortcuts import model_to_dict
+import datetime
 
 load_dotenv()
 app = Flask(__name__)
 
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
+print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+    
+    class Meta:
+        database = mydb
+    
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 @app.route('/')
 def home():
@@ -13,3 +30,21 @@ def home():
 @app.route('/hobbies')
 def hobbies():
     return render_template('hobbies.html', title="My Hobbies - MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_timeline_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_timeline_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
